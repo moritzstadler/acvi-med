619eb1441967fe985347be89f4c5cfcb07fbe81f
Moritz Stadler <moritz.j.j.stadler@gmail.com>
2022-02-06 10:32:46 +0100

    create index for GT

--- src/main/java/SqlConnector.java
+++ src/main/java/SqlConnector.java
@@ -96,7 +96,7 @@ public class SqlConnector {
         for (String formatName : formatNames) {
             for (String formatType : formatTypes) {
                 String colName = "format_" + formatName + "_" + formatType;
-                if (formatType.equals("gt")) {
+                if (formatType.toLowerCase().equals("gt")) {
                     cols.add(String.format("%s %s", colName, "VARCHAR(255)"));
                 } else {
                     cols.add(String.format("%s %s", colName, "TEXT"));
