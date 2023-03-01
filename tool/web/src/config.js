8e80bcdea555a28716c33ef9e0526c3766d6490b
Moritz Stadler <moritz.j.j.stadler@gmail.com>
2023-03-01 20:15:02 +0100

    change config

--- tool/web/src/config.js
+++ tool/web/src/config.js
@@ -1,6 +1,6 @@
 export const Config = { 
-	/*apiBaseUrl: "https://derma-genomics.meduniwien.ac.at" + "/api",*/
-	apiBaseUrl: "http://localhost:8080",
+	apiBaseUrl: "https://derma-genomics.meduniwien.ac.at" + "/api",
+	//apiBaseUrl: "http://localhost:8080",
 	appBaseUrl: "https://" + /*window.location.hostname*/ "derma-genomics.meduniwien.ac.at"
 };
 
