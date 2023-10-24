c76bc8d0f420fa45f1eefe98b24f6c2679f02edc
Moritz Stadler <moritz.j.j.stadler@gmail.com>
2023-10-24 19:21:01 +0200

    add dbg output

--- tool/core/src/main/java/at/ac/meduniwien/vcfvisualize/rest/SecondaryFindingsLoader.java
+++ tool/core/src/main/java/at/ac/meduniwien/vcfvisualize/rest/SecondaryFindingsLoader.java
@@ -93,8 +93,8 @@ public class SecondaryFindingsLoader {
                 List<GenomicPosition> clinvarResult = clinvar.findPathogenics(variant.getChrom(), variant.getPos(), variant.getAlt());
                 boolean clinvarPathogenicFound = clinvarResult != null && clinvarResult.size() > 0;
                 boolean clinvarPathogenicAnnotated = false;
-                if (variant.getInfo() != null && variant.getInfo().containsKey("info_csq_clinvar_clnsig") && variant.getInfo().get("info_csq_clinvar_clnsig") != null) {
-                    clinvarPathogenicAnnotated = variant.getInfo().get("info_csq_clinvar_clnsig").toLowerCase().contains("pathogenic"); //targets both pathogenic and likely pathogenic
+                if (variant.getInfo() != null && variant.getInfo().containsKey("info_csq_clinvar_clnsigconf") && variant.getInfo().get("info_csq_clinvar_clnsigconf") != null) {
+                    clinvarPathogenicAnnotated = variant.getInfo().get("info_csq_clinvar_clnsigconf").toLowerCase().contains("pathogenic"); //targets both pathogenic and likely pathogenic
                 }
                 boolean clinvarPositive = clinvarPathogenicFound || clinvarPathogenicAnnotated;
 
